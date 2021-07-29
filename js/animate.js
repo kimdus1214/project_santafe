bb1bd6126204f84bb1461d650d73feae3188c5d6
PhooYoon <cjfdnjs1994@naver.com>
2021-07-29 23:05:32 +0900

    0729_branch add

--- js/animate.js
+++ js/animate.js
@@ -1,41 +1,52 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+  $(window).scroll(function () {
     var techScroll = $(window).scrollTop();
     console.log(techScroll);
-    if(techScroll > 4800){
-        $('#techAnimateImg').stop().animate({
-            left:0
-        },1000);
+      var techOffset = $("#technologySection").offset();
+      var perOffset = $("#performanceSection").offset();
+    if (techScroll > techOffset.top - 650) {
+      $("#techAnimateImg").stop().animate(
+        {
+          left: 0,
+        },
+        500
+      );
     } else {
-        $('#techAnimateImg').stop().animate({
-            left: -1000
-        });
+      $("#techAnimateImg").stop().animate({
+        left: -1000,
+      });
     }
- 
-    if(techScroll >= 5800){
-        $('#perforAnimateImg').stop().animate({
-            right : 0
-        },1000);
-    } else{
-        $('#perforAnimateImg').stop().animate({
-            right : -1000
-        });
-    };
 
-    if(techScroll == 0){
-        $('#topBtn').css({
-            display : 'none'
-        });
-    } else{
-        $('#topBtn').css({
-            display : 'block'
-        });
+    if (techScroll >= perOffset.top - 650) {
+      $("#perforAnimateImg").stop().animate(
+        {
+          right: 0,
+        },
+        500
+      );
+    } else {
+      $("#perforAnimateImg").stop().animate({
+        right: -1000,
+      });
     }
-    });
 
-    $('#topBtn').click(function(){
-        $('body, html').animate({
-            scrollTop : 0,
-        },1000);
-    });
+    if (techScroll == 0) {
+      $("#topBtn").css({
+        display: "none",
+      });
+    } else {
+      $("#topBtn").css({
+        display: "block",
+      });
+    }
+  });
+
+  $("#topBtn").click(function () {
+    $("body, html").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+  });
 });
