b82dd4961278562e2368a6a4cb3ca298989c02e0
yomii <kimdus1214@gmail.com>
2021-07-29 14:09:54 +0900

    tab.js 추가

---
+++ js/safetyTab.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $('#safetyContent > li').each(function(index){
+        $(this).attr('data-index',index);
+    });
+    $('#safetyVideoWrap > video').each(function(index){
+        $(this).attr('data-index',index);
+    });
+    $('#safetyTab > li').each(function(index){
+        $(this).attr('data-index',index);
+    }).click(function(){
+        var safetyTab = $(this).attr('data-index');
+         $('#safetyContent > li[data-index=' + safetyTab + ']').css('display','block');
+         $('#safetyContent > li[data-index!=' + safetyTab + ']').css('display','none');
+         $('#safetyVideoWrap > video[data-index=' + safetyTab + ']').css('display','block');
+         $('#safetyVideoWrap > video[data-index!=' + safetyTab + ']').css('display','none');
+         $('#safetyTab > li[data-index=' + safetyTab + ']').addClass('safetyOn');
+         $('#safetyTab > li[data-index!=' + safetyTab + ']').removeClass('safetyOn');
+    });
+
+
+  });
