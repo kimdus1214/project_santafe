3076d6262f2ffc1712435c144b4220774f8e61fa
kimdus1214 <kimdus1214@gmail.com>
2021-08-03 17:45:47 +0900

    0803 complete

--- js/animate.js
+++ js/animate.js
@@ -1,41 +1,53 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-    var techScroll = $(window).scrollTop();
-    console.log(techScroll);
-    if(techScroll > 4800){
-        $('#techAnimateImg').stop().animate({
-            left:0
-        },1000);
-    } else {
-        $('#techAnimateImg').stop().animate({
-            left: -1000
+$(document).ready(function () {
+    $(window).scroll(function () {
+      var techScroll = $(window).scrollTop();
+      console.log(techScroll);
+        var techOffset = $("#technologySection").offset();
+        var perOffset = $("#performanceSection").offset();
+      if (techScroll > techOffset.top - 650) {
+        $("#techAnimateImg").stop().animate(
+          {
+            left: 0,
+          },
+          500
+        );
+      } else {
+        $("#techAnimateImg").stop().animate({
+          left: -1000,
         });
-    }
- 
-    if(techScroll >= 5800){
-        $('#perforAnimateImg').stop().animate({
-            right : 0
-        },1000);
-    } else{
-        $('#perforAnimateImg').stop().animate({
-            right : -1000
+      }
+  
+      if (techScroll >= perOffset.top - 650) {
+        $("#perforAnimateImg").stop().animate(
+          {
+            right: 0,
+          },
+          500
+        );
+      } else {
+        $("#perforAnimateImg").stop().animate({
+          right: -1000,
         });
-    };
-
-    if(techScroll == 0){
-        $('#topBtn').css({
-            display : 'none'
+      }
+  
+      if (techScroll == 0) {
+        $("#topBtn").css({
+          display: "none",
         });
-    } else{
-        $('#topBtn').css({
-            display : 'block'
+      } else {
+        $("#topBtn").css({
+          display: "block",
         });
-    }
+      }
     });
-
-    $('#topBtn').click(function(){
-        $('body, html').animate({
-            scrollTop : 0,
-        },1000);
+  
+    $("#topBtn").click(function () {
+      $("body, html").animate(
+        {
+          scrollTop: 0,
+        },
+        1000
+      );
     });
-});
+  });
+  
---
+++ js/tab.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+
+  // menuBtn 
+  $('#menuBtn').click(function () {
+    if ($('#tabMenuWrap').hasClass('tabMenuWrapNone')) {
+      $('#tabMenuWrap').addClass('tabMenuWrapAction').removeClass('tabMenuWrapNone');
+      $('#tabMenu').stop().animate({
+        left : 0
+      }, 500);
+      $('#tabMenuWrap, #tabHeader').on('scroll touchmove mousewheel', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+      });
+
+    } else {
+      $('#tabMenuWrap').addClass('tabMenuWrapNone');
+      $('#tabMenu').stop().animate({
+        left : '-60%'
+      }, 500);
+      $('#tabMenuWrap, #tabHeader').off('scroll touchmove mousewheel'); 
+    }
+
+    if ($('#menuBtn svg').hasClass('fa-bars')) {
+      $('#menuBtn svg').addClass('fa-times').removeClass('fa-bars');
+    } else {
+      $('#menuBtn svg').addClass('fa-bars')
+    }
+
+
+  });
+ // menuBtn End
+  
+  // menuBtnLeft
+  $('#menuBtnLeft').click(function () {
+    if ($('#tabRightMenuWrap').hasClass('tabMenuWrapNone')) {
+      $('#tabRightMenuWrap').addClass('tabMenuWrapAction').removeClass('tabMenuWrapNone');
+      $('#tabRightMenuBox').stop().animate({
+        right: 0
+      }, 500);
+      $('#tabRightMenuWrap, #tabHeader').on('scroll touchmove mousewheel', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+      });
+    } else {
+      $('#tabRightMenuWrap').addClass('tabMenuWrapNone');
+      $('#tabRightMenuBox').stop().animate({
+        right: '-40%'
+      }, 500);
+      $('#tabRightMenuWrap, #tabHeader').off('scroll touchmove mousewheel'); 
+    }
+    
+    if ($('#menuBtnLeft svg').hasClass('fa-plus')) {
+      $('#menuBtnLeft svg').addClass('fa-times').removeClass('fa-plus');
+    } else {
+      $('#menuBtnLeft svg').addClass('fa-plus')
+    }
+
+
+  });
+  // menuBtnLeft End
+});
